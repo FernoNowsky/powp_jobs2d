5351fbce0ea29db85879f9a1acdf445008f491c2
FernoNowsky <246327@edu.p.lodz.pl>
2025-12-03 19:53:44 +0100

    Added drivers to use LineDrawerAdapter with specific line type

--- src/test/java/edu/kis/powp/jobs2d/TestJobs2dPatterns.java
+++ src/test/java/edu/kis/powp/jobs2d/TestJobs2dPatterns.java
@@ -7,7 +7,9 @@ import java.util.logging.Logger;
 
 import edu.kis.legacy.drawer.panel.DefaultDrawerFrame;
 import edu.kis.legacy.drawer.panel.DrawPanelController;
+import edu.kis.legacy.drawer.shape.LineFactory;
 import edu.kis.powp.appbase.Application;
+import edu.kis.powp.jobs2d.drivers.adapter.LineDrawerAdapter;
 import edu.kis.powp.jobs2d.drivers.adapter.SetAndOperateToDrawerAdapter;
 import edu.kis.powp.jobs2d.events.SelectChangeVisibleOptionListener;
 import edu.kis.powp.jobs2d.events.SelectTestFigure2OptionListener;
@@ -45,6 +47,15 @@ public class TestJobs2dPatterns {
 
 		Job2dDriver testDriver = new SetAndOperateToDrawerAdapter();
 		DriverFeature.addDriver("Buggy Simulator", testDriver);
+
+        Job2dDriver basicLineDriver = new LineDrawerAdapter(LineFactory.getBasicLine());
+        DriverFeature.addDriver("Basic Line Simulator", basicLineDriver);
+
+        Job2dDriver dottedLineDriver = new LineDrawerAdapter(LineFactory.getDottedLine());
+        DriverFeature.addDriver("Dotted Line Simulator", dottedLineDriver);
+
+        Job2dDriver specialLineDriver = new LineDrawerAdapter(LineFactory.getSpecialLine());
+        DriverFeature.addDriver("Special Line Simulator", specialLineDriver);
 
 		DriverFeature.updateDriverInfo();
 	}
