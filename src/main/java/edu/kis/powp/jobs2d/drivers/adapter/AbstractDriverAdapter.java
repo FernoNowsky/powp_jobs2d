d24f5a8c19f689b35e96849d095c83fc4a3be80a
FernoNowsky <246327@edu.p.lodz.pl>
2025-12-03 22:45:12 +0100

    Created AbstractDriverAdapter

---
+++ src/main/java/edu/kis/powp/jobs2d/drivers/adapter/AbstractDriverAdapter.java
@@ -0,0 +1,29 @@
+package edu.kis.powp.jobs2d.drivers.adapter;
+
+import edu.kis.powp.jobs2d.AbstractDriver;
+import edu.kis.powp.jobs2d.Job2dDriver;
+import edu.kis.powp.jobs2d.features.DriverFeature;
+
+public class AbstractDriverAdapter extends AbstractDriver {
+
+    public AbstractDriverAdapter() {
+        super(0, 0);
+    }
+
+    public AbstractDriverAdapter(int x, int y) {
+        super(x, y);
+    }
+
+    @Override
+    public void operateTo(int x, int y) {
+        Job2dDriver currentDriver = DriverFeature.getDriverManager().getCurrentDriver();
+        currentDriver.setPosition(getX(), getY());
+        currentDriver.operateTo(x, y);
+        setPosition(x, y);
+    }
+
+    @Override
+    public String toString() {
+        return "@Q!$!@$!#@$(*#@&Q(%^*#@";
+    }
+}
